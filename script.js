66b5b522c7758cb4f0f94cdde8e1851afbc0738c
Denis-Lazarev <denis.lazarev.w@gmail.com>
2019-01-26 17:50:06 +0200

    2й довабления правильного ответа

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ' use strict ';
 
-let money = prompt('Ваш бюджет на месяц?, ""');
-let time = prompt('Введите дату в формате YYYY-MM-DD, ""');
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     money,
